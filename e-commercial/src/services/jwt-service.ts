5cede18a1de6bd1a2471bd81fec65efe446e3c8c
Vu Hoang Anh <vuhoanganh1821@gmail.com>
2022-11-29 18:12:33 +0700

    BE-01 | fix authorization

--- e-commercial/src/services/jwt-service.ts
+++ e-commercial/src/services/jwt-service.ts
@@ -15,49 +15,45 @@ export class JWTService {
   @inject(TokenServiceBindings.TOKEN_EXPIRES_IN)
   public readonly expiresSecret: string;
 
-  async generateToken(customerProfile: UserProfile): Promise<string> {
-    if (!customerProfile) {
+  async generateToken(userProfile: UserProfile): Promise<string> {
+    if (!userProfile) {
       throw new HttpErrors.Unauthorized(
-        'Error while generating token :customerProfile is null'
-      )
+        'Error while generating token :userProfile is null',
+      );
     }
     let token = '';
     try {
-      token = await signAsync(customerProfile, this.jwtSecret, {
-        expiresIn: this.expiresSecret
+      token = await signAsync(userProfile, this.jwtSecret, {
+        expiresIn: this.expiresSecret,
       });
       return token;
     } catch (err) {
-      throw new HttpErrors.Unauthorized(
-        `error generating token ${err}`
-      )
+      throw new HttpErrors.Unauthorized(`error generating token ${err}`);
     }
   }
 
   async verifyToken(token: string): Promise<UserProfile> {
-
     if (!token) {
       throw new HttpErrors.Unauthorized(
         `Error verifying token:'token' is null`
-      )
-    };
+      );
+    }
 
-    let customerProfile: UserProfile;
+    let userProfile: UserProfile;
     try {
       const decryptedToken = await verifyAsync(token, this.jwtSecret);
-      customerProfile = Object.assign(
+      userProfile = Object.assign(
         {[securityId]: '', id: '', name: '', roles: []},
         {
           [securityId]: decryptedToken.id,
           id: decryptedToken.id,
           name: decryptedToken.name,
-          roles: decryptedToken.roles
+          roles: decryptedToken.roles,
         },
       );
+    } catch (err) {
+      throw new HttpErrors.Unauthorized(`Error verifying token:${err.message}`);
     }
-    catch (err) {
-      throw new HttpErrors.Unauthorized(`Error verifying token:${err.message}`)
-    }
-    return customerProfile;
+    return userProfile;
   }
 }
